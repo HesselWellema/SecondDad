d3ae04aa8db4eb988d2fb5d594b29da4e4927386
Hessel Wellema <Hessel Wellema>
2016-10-26 14:02:56 +0200

    commit

--- app.js
+++ app.js
@@ -7,23 +7,25 @@
 6 var appSecret = process.env.MY_APP_SECRET || "Missing your app secret"; 
 7 
  
-8 // Create bot and add dialogs 
-9 var bot = new builder.BotConnectorBot 
-10 ({appId: process.env.MY_APP_ID, appSecret: process.env.MY_APP_SECRET}); 
-11 bot.add('/', new builder.SimpleDialog( function (session) { 
-12 session.send('Hello World'); 
-13 })); 
-14 
- 
-15 // Setup Restify Server 
-16 var server = restify.createServer(); 
-17 server.post('/api/messages', bot.verifyBotFramework(), bot.listen()); 
-18 server.listen(process.env.port || 3000, function () { 
-19 console.log('%s listening to %s', server.name, server.url); 
-20 }); 
-21 
- 
-22 server.get('/', restify.serveStatic({ 
-23     directory: __dirname, 
-24     default: '/index.html' 
-25 })); 
+server.get('/', restify.serveStatic({
+ directory: __dirname,
+ default: '/index.html'
+}));
+
+var bot = new builder.TextBot();
+bot.add('/', function (session) {
+    if (!session.userData.name) {
+        session.beginDialog('/profile');
+    } else {
+        session.send('Hello %s!', session.userData.name);
+    }
+});
+bot.add('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
