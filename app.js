324e2c0705e88caa41182af41a70ce11b1805ed8
Hessel Wellema <Hessel Wellema>
2016-10-26 14:06:16 +0200

    commit

--- app.js
+++ app.js
@@ -1,31 +1,21 @@
-//Add your requirements 
-2 var restify = require('restify'); 
-3 var builder = require('botbuilder'); 
-4 
- 
-5 var appId = process.env.MY_APP_ID || "Missing your app ID"; 
-6 var appSecret = process.env.MY_APP_SECRET || "Missing your app secret"; 
-7 
- 
-server.get('/', restify.serveStatic({
- directory: __dirname,
- default: '/index.html'
-}));
-
-var bot = new builder.TextBot();
-bot.add('/', function (session) {
-    if (!session.userData.name) {
-        session.beginDialog('/profile');
-    } else {
-        session.send('Hello %s!', session.userData.name);
-    }
+// Add your requirements
+var restify = require('restify'); 
+var builder = require('botbuilder'); 
+var appId = process.env.MY_APP_ID,
+var appPassword = process.env.MY_APP_PASSWORD;
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.PORT || 3000, function() 
+{
+   console.log('%s listening to %s', server.name, server.url); 
 });
-bot.add('/profile', [
-    function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
-    },
-    function (session, results) {
-        session.userData.name = results.response;
-        session.endDialog();
-    }
-]);
+
+// Create chat bot
+var connector = new builder.ChatConnector
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+// Create bot dialogs
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
