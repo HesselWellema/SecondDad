1b96e96c28e1389cc2768f77aad05ff324982949
Hessel Wellema <Hessel Wellema>
2016-11-23 13:53:07 +0100

    commit

--- app.js
+++ app.js
@@ -614,7 +614,7 @@ bot.dialog('/astrologie', [
             .attachments([
             new builder.HeroCard(session)
             //.title(sign.name + " (element: " + sign.about.element +  ")")
-            .text(sign.name + " (element: " + sign.about.element +  ")" + "  \n" + "Strengths: " + sterktes + "  \n" + "Weaknesses: " + "  \n" + zwaktes)
+            .text(sign.name + " (element: " + sign.about.element +  ")" + "  \n" + "Strengths: " + sterktes + "  \n" + "Weaknesses: " + zwaktes)
             //.images([builder.CardImage.create(session, 'http://65.media.tumblr.com/avatar_f349b770a991_128.png')])
             //.tap(builder.CardAction.openUrl(session, conditions.forecast_url))
             ]);
